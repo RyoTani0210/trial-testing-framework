fc0d633230c2154469b714d61351e23dc51bc322
RyoTani0210 <ryo.taniguchi@gmail.com>
2024-07-27 20:23:37 +0900

    add coverage test functon

--- moduleOne.py
+++ moduleOne.py
@@ -6,3 +6,14 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
+
+def decision(a, b):
+    if a & b:
+        return 1
+    elif a:
+        return 2
+    elif b:
+        return 3
+    else:
+        return 4
+
